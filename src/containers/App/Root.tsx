566f738cd3cd3c276a4071455066830d27b40134
dime <dime@twowls.org>
2022-12-12 19:19:47 +0400

    Show user name in document title

--- src/containers/App/Root.tsx
+++ src/containers/App/Root.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Box, CssBaseline} from "@mui/material";
 import Sidebar from "./Sidebar";
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
+import {useLoginUser} from "../../hooks";
 
 const Root: React.FC = () => {
+    const loginUser = useLoginUser()
+    const location = useLocation()
+
+    useEffect(() => {
+        let title = ""
+
+        if (loginUser.id) {
+            title = loginUser.id + " – "
+        }
+
+        if (location.pathname === "/") {
+            title += "Homepage"
+        } else {
+            const p = location.pathname.substring(1).split("/", 1)
+            if (p.length > 0) {
+                title += p[0].charAt(0).toUpperCase()
+                title += p[0].substring(1)
+            }
+        }
+
+        document.title = title
+    }, [loginUser, location])
+
     return (
         <Box sx={{display: "flex"}}>
             <CssBaseline/>
